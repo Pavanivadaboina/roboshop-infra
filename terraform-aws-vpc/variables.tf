996c42a93d388e37a9435280dc359b811ff90fbc
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-27 23:24:12 +0530

    ansible code

---
+++ terraform-aws-vpc/variables.tf
@@ -0,0 +1,42 @@
+variable "cidr_block" {
+}
+
+variable "enable_dns_hostnames" {
+  default = true
+}
+
+variable "enable_dns_support" {
+  default = true
+}
+
+variable "common_tags" {
+
+}
+
+variable "vpc_tags" {
+  
+}
+
+variable "igw_tags"{
+
+}
+
+variable "public_subnet_names" {
+
+}
+
+variable "public_subnet_cidr" {
+
+}
+
+variable "azs" {
+
+}
+
+variable "private_subnet_names" {
+
+}
+
+variable "private_subnet_cidr" {
+
+}
