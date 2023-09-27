fb52ed634b6fc4af615118fa9b608677ca7ee27a
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-27 23:51:28 +0530

    ansible code

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,19 @@
 module "this" {
-  source    = "../terraform-aws-vpc"
-  cidr_block  = var.cidr_block
-  common_tags = var.common_tags
-  vpc_tags    = var.vpc_tags
+  source               = "../module-vpc"
+  cidr_block           = var.cidr_block
+  common_tags          = var.common_tags
+  vpc_tags             = var.vpc_tags
   enable_dns_hostnames = var.enable_dns_hostnames
-  enable_dns_support = var.enable_dns_support
-  igw_tags = var.igw_tags
-
-  #public subnet creation
-  public_subnet_cidr = var.public_subnet_cidr
-  azs = var.azs
-  public_subnet_names = var.public_subnet_names
-
-  #private subnet creation
-  private_subnet_cidr = var.private_subnet_cidr
-  private_subnet_names = var.private_subnet_names
+  enable_dns_support   = var.enable_dns_support
 }
+#  igw_tags = var.igw_tags
+#
+#  #public subnet creation
+#  public_subnet_cidr = var.public_subnet_cidr
+#  azs = var.azs
+#  public_subnet_names = var.public_subnet_names
+#
+#  #private subnet creation
+#  private_subnet_cidr = var.private_subnet_cidr
+#  private_subnet_names = var.private_subnet_names
+#}
