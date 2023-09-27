f59d0c951ea03672ef8bcfea3590e377c0fdda42
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-28 00:23:50 +0530

    ansible code

---
+++ module-vpc/var.tf
@@ -0,0 +1,18 @@
+variable "cidr_block" {
+}
+
+variable "enable_dns_hostnames" {
+  default = true
+}
+
+variable "enable_dns_support" {
+  default = true
+}
+
+variable "common_tags" {
+
+}
+
+variable "vpc_tags" {
+
+}
