d0563be843d522e47b724f3416349c6913ab9a21
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-28 00:27:24 +0530

    ansible code

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 module "this" {
-  source               = "../module-vpc"
+  source               = "./module-vpc"
   cidr_block           = var.cidr_block
   common_tags          = var.common_tags
   vpc_tags             = var.vpc_tags
