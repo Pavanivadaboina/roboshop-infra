a236ce8f5ece3e61358b045ef07096cbe6994990
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-28 12:50:23 +0530

    terraform vpc code

--- module-vpc/main.tf
+++ module-vpc/main.tf
@@ -92,4 +92,5 @@ resource "aws_route_table_association" "private" {
   count = length(var.private_subnet_cidr)
   subnet_id      = element(aws_subnet.private[*].id, count.index )
   route_table_id = aws_route_table.private.id
+
 }
