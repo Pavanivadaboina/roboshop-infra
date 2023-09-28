d6ce76b6162a47452d42f60bf448d0a988cc686b
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-28 11:42:17 +0530

    ansible code

--- vpc.tf
+++ vpc.tf
@@ -5,15 +5,19 @@ module "this" {
   vpc_tags             = var.vpc_tags
   enable_dns_hostnames = var.enable_dns_hostnames
   enable_dns_support   = var.enable_dns_support
+
+  igw_tags = var.igw_tags
+
+  #public subnet creation
+  public_subnet_cidr = var.public_subnet_cidr
+  azs = var.azs
+  public_subnet_names = var.public_subnet_names
+
+  #private subnet creation
+  private_subnet_cidr = var.private_subnet_cidr
+  private_subnet_names = var.private_subnet_names
+
+  #database subnet creation
+  database_subnet_cidr = var.database_subnet_cidr
+  database_subnet_names = var.database_subnet_names
 }
-#  igw_tags = var.igw_tags
-#
-#  #public subnet creation
-#  public_subnet_cidr = var.public_subnet_cidr
-#  azs = var.azs
-#  public_subnet_names = var.public_subnet_names
-#
-#  #private subnet creation
-#  private_subnet_cidr = var.private_subnet_cidr
-#  private_subnet_names = var.private_subnet_names
-#}
